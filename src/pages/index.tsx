d947e643cfa7d32835dc4d11094162c3f1dad57b
Torsak Tongkum <torsak@freshconsulting.com>
2021-04-01 10:09:34 +0700

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+type People = {
+  name?: string
+}
+
+const people = {
+  name: "Alin",
+}
+
+function hi(person: People) {
+  return person.name
+}
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi {hi(people)}</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
